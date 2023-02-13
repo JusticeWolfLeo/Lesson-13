885395d1fe6dadfc4938a8f265dfb9aaea3f0813
JusticeWolfLeo <115044383+JusticeWolfLeo@users.noreply.github.com>
2023-02-13 08:36:52 +0300

    Add files via upload

--- CopyFile.java
+++ CopyFile.java
@@ -3,11 +3,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class CopyFile {
     public static void main(String[] args) {
         copyFile("C:/Users/Veronika/IdeaProjects/Lesson_13/src/Doklad.txt", "C:/Users/Veronika/IdeaProjects/Lesson_13/src/Doklad_copy.txt");
+        logging();
     }
 
     private static void copyFile(String source, String target) {
@@ -25,10 +27,17 @@ public class CopyFile {
         }
     }
 
-    public static void logging(String[] args) {
-        Date date = new Date();
-        try (FileWriter writer = new FileWriter("log.txt", true)) { //еще хрен знает где он создастся...
-            String text = date + "был скопирован файл" + source + "→" + target; // у меня просто закончились идеи, я знаю, что это хрень, SOS
+    public static void logging() {
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        String string = "C:/Users/Veronika/IdeaProjects/Lesson_13/src/Doklad.txt";
+        String[] parts = string.split("/");
+        String part1 = parts[6];
+        String string2 ="C:/Users/Veronika/IdeaProjects/Lesson_13/src/Doklad_copy.txt";
+        String[] tparts = string.split("/");
+        String tpart1 = tparts[6];
+        try (FileWriter writer = new FileWriter("log.txt", true)) {
+            String text = date +" "+ time + "был скопирован файл" + parts[6] + "→" + tparts[6];
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
